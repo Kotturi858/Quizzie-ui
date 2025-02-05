213ea3db5547c8707e4beb4c0ea13bdd598e53c3
Kotturi858 <kotturi858bharath@gmail.com>
2025-02-05 20:27:47 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ThemeService } from './shared/services/theme.service';
-import { InstructionsComponent } from './pages/instructions/instructions.component';
-import { QCardComponent } from './shared/components/q-card/q-card.component';
-import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, DashboardComponent, InstructionsComponent, QCardComponent,LoginComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  theme = inject(ThemeService)
+  theme = inject(ThemeService);
 }
