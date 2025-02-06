ad9b83ca0448dbb633ed059a926d09d81d49667d
Kotturi858 <kotturi858bharath@gmail.com>
2025-02-06 20:44:19 +0530

    added register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ProgressBar } from 'primeng/progressbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-register',
-  imports: [ProgressBar],
+  imports: [
+    ButtonModule,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ProgressBarModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  visibility = true;
+  constructor(private readonly router: Router) {}
 
+  toggleVisibility() {
+    this.visibility = !this.visibility;
+  }
+
+  register() {
+    this.router.navigate(['/login']);
+  }
 }
